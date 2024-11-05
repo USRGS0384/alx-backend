cb30d846e29acb1dffddff4d801b98f033999ade
Philip Ajuong Deng <philipajuong@gmail.com>
2024-11-05 23:05:26 +0000

    0x02. i18n

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,44 +1,44 @@
 #!/usr/bin/env python3
 """
-Flask app
+Flask app with get_locale function using request.accept_languages.
 """
-from flask import (
-    Flask,
-    render_template,
-    request
-)
-from flask_babel import Babel
 
-
-class Config(object):
-    """
-    Configuration for Babel
-    """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+from typing import List
 
 app = Flask(__name__)
+
+
+class Config:
+    """
+    Setup babel configuration
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
 app.config.from_object(Config)
+
 babel = Babel(app)
 
-
+# Function used for marking strings for translation
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     """
-    Select and return best language match based on supported languages
+    Determines the best match for the user's preferred language.
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/', strict_slashes=False)
-def index() -> str:
+@app.route('/')
+def index():
     """
-    Handles / route
+    Renders the index.html template.
     """
     return render_template('3-index.html')
 
 
 if __name__ == "__main__":
-    app.run(port="5000", host="0.0.0.0", debug=True)
+    app.run(host='0.0.0.0', port=5000)
