7cb702d339d83857ff435d98344635e89474d704
Philip Ajuong Deng <philipajuong@gmail.com>
2024-11-07 12:51:29 +0000

    0x02. i18n

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -6,7 +6,7 @@ from flask import (
     render_template,
     request
 )
-from flask_babel import Babel
+from flask_babel import Babel, _
 
 app = Flask(__name__)
 babel = Babel(app)
@@ -34,7 +34,7 @@ def get_locale():
 def index():
     """ return a hello world page
     """
-    return render_template('3-index.html')
+     return render_template('3-index.html', title=_("home_title"), header=_("home_header"))
 
 
 if __name__ == '__main__':
